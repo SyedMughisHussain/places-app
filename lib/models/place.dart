093a96c67cdee429168cf3ee9b12b6d838e0609e
SyedMuhgisHussain <syedmughis4541@gmail.com>
2023-07-04 11:37:55 +0500

    First Commit for Pushing code

---
+++ lib/models/place.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+class Place {
+  final String id;
+  final String title;
+  final PlaceLocation location;
+  final File image;
+
+  Place({
+    required this.id,
+    required this.title,
+    required this.location,
+    required this.image,
+  });
+}
+
+class PlaceLocation {
+  final double latitide;
+  final double longitude;
+  final String? address;
+
+  PlaceLocation({
+    required this.latitide,
+    required this.longitude,
+    this.address,
+  });
+}
